ba09a3306c733b58023cdb58860b3cb149e01049
Anoop Kumar Chaurasia <anoop26667@gmail.com>
2018-12-28 18:12:09 +0530

    Update app.js
    Separate info and error log

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const pm2 = require('pm2');
 const pmx = require('pmx');
-const packageJSON =  require('./package');
+const packageJSON = require('./package');
 const conf = pmx.initModule();
 const elasticLibModule = require('./libs/elasticsearch');
 const elasticLib = new elasticLibModule(conf.elasticsearch_host, conf.elasticsearch_index);
@@ -13,18 +13,45 @@ const elasticLib = new elasticLibModule(conf.elasticsearch_host, conf.elasticsea
  *          $ pm2 install .
  */
 
-pm2.Client.launchBus(function(err, bus) {
-    if(err)
+pm2.Client.launchBus(function (err, bus) {
+    if (err)
         return console.error(err);
-    bus.on('log:*', function(type, packet) {
-        if ((conf.app_name != 'all' && conf.app_name && conf.app_name != packet.process.name) || packet.process.name === packageJSON.name) return false;
 
-        if (typeof(packet.data) == 'string')
-            packet.data = packet.data.replace(/(\r\n|\n|\r)/gm,'');
+    bus.on("log:out", function (packet) {
+        if (packet.process.name === packageJSON.name) {
+            return;
+        }
+
         elasticLib.add({
-            message : packet.data,
-            application : packet.process.name,
-            process_id : packet.process.pm_id,
-        }).catch((e) => {console.error(e)});
+            message: packet.data,
+            log_level: "INFO",
+            application: packet.process.name,
+            process_id: packet.process.pm_id,
+            type: "logger"
+        }).catch((e) => {
+            console.error(e)
+        });
+    });
+
+    bus.on("log:err", function (packet) {
+        if (packet.process.name === packageJSON.name) {
+            return;
+        }
+
+        elasticLib.add({
+            message: packet.data,
+            log_level: "ERROR",
+            application: packet.process.name,
+            process_id: packet.process.pm_id,
+            type: "logger"
+        }).catch((e) => {
+            console.error(e)
+        });
+
+    });
+
+    bus.on('close', function () {
+        console.log('PM2 Logstash Connector: Bus closed');
+        pm2.disconnectBus();
     });
 });
